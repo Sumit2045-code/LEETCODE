01e7e8b6f4eedaaf892419445db91f28ded0635c
Sumit2045-code <sumitcodes2045@gmail.com>
2025-10-07 12:40:54 +0530

    Time: 431 ms (5.02%), Space: 23.8 MB (55.47%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int count = 0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if (nums[i]!= 0)
-            {
-                swap(nums[i],nums[count]);
-                count ++;
+        int n = nums.size();
+        for(int i=0;i<n-1;i++){  
+         for(int j=0;j<n-1-i;j++){
+            if (nums[j]==0){
+                swap(nums[j],nums[j+1]);
             }
-        }
+         }
+    }
     }
 };

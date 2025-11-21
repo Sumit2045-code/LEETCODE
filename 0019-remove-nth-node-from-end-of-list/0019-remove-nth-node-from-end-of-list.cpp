0507520eb29c509428eae267d5beca51aca3e4f4
Sumit2045-code <sumitcodes2045@gmail.com>
2025-11-21 10:41:58 +0530

    Time: 0 ms (100%), Space: 15 MB (59.34%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-      int len = 0;
-      ListNode* temp = head;
-      while(temp!=NULL){
-        len++;
-        temp = temp->next;
-      }
-      if(n==len){//this is for handling the case jha pe hme jis node ko htana ho whi agr first node hua toh agr temp hi null hoga to temp ka next to error show krega isliye hme isse handle krna pdega 
-        head = head->next;
-        return head;
-      } 
-      int m = len-n+1;
-      int idx = m-1;
-      temp = head;
-      for(int i=1;i<=idx-1;i++){
-        temp = temp->next;
-      } 
-      temp->next = temp->next->next;
-      return head;
+          ListNode* slow = head;
+          ListNode* fast = head;
+          //traverse fast to n+1 steps 
+          for(int i =1;i<=n+1;i++){
+            if (fast==NULL) return head->next;
+            fast = fast->next;
+          }
+          //move slow and fast at same pace 
+          while(fast!=NULL){
+            slow = slow->next;
+            fast = fast->next;
+          }
+          //now the slow is exactly at the position where right node is to be deleted which is the nth node from last
+          slow->next = slow->next->next;
+          return head;
     }
 };

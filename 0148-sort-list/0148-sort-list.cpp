5e10e7ba637cd345c283dd04e016a5ca864c4f93
Sumit2045-code <sumitcodes2045@gmail.com>
2025-11-22 17:26:01 +0530

    Time: 47 ms (35.96%), Space: 75.8 MB (19.12%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,40 @@
+  class Solution { 
+public:
+    ListNode* merge(ListNode* a, ListNode* b) {
+        ListNode* c = new ListNode(100);
+        ListNode* temp = c;
+        while(a!=NULL & b!=NULL){
+            if(a->val<= b->val){
+                temp->next = a;
+                a = a->next;
+                temp = temp->next;
+            }
+            else{// b->val<a->val
+                temp->next = b;
+                b = b->next;
+                temp = temp->next;
+            }
+        }
+        if(a==NULL)  temp->next = b;
+        else temp->next = a ;
+        return c->next;
+    }
+    ListNode* sortList(ListNode* head) {
+        if(head==NULL || head->next==NULL) return head;
+       // first step is to find the left middle of the linked list 
+       ListNode* slow = head;
+       ListNode* fast = head;
+        while(fast->next!=NULL && fast->next->next!=NULL){// for left middle
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        // now slow is at left middle 
+        ListNode* a = head;
+        ListNode* b = slow->next;
+        slow->next = NULL;
+        a = sortList(a);
+        b = sortList(b);
+        ListNode* c = merge(a,b);
+        return c;
+    }
+};

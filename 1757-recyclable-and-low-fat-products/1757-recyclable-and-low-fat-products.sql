3d779f4801042ed08d6510b20d364a6d4a97f684
Sumit2045-code <sumitcodes2045@gmail.com>
2026-02-20 07:31:22 +0530

    Time: 583 ms (18.57%), Space: 0B (100%) - LeetHub

---
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT product_id
+FROM Products
+WHERE low_fats='Y' AND recyclable='Y';
+-- SQL ME STRINGS LIKE 'Y' ya 'N' KO HMESHA SINGLE QUOTES ME LIKHTE HAIN   

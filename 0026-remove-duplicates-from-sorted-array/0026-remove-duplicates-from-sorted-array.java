445894033ea657b787ec77343ff05342f13390a6
Sumit2045-code <sumitcodes2045@gmail.com>
2026-03-15 00:25:53 +0530

    Time: 1 ms (79.19%), Space: 46.8 MB (59.75%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,15 +1,24 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         int n = nums.length;
-        ArrayList<Integer> ans = new ArrayList<>();
+        // ArrayList<Integer> ans = new ArrayList<>();
+        // for(int i=0; i<n; i++){
+        //     if(!ans.contains(nums[i])){
+        //             ans.add(nums[i]);
+        //         }
+        //     }
+        // for(int i=0 ; i<ans.size();i++){
+        //     nums[i]=ans.get(i);
+        // }
+        // return ans.size();
+
+        int uniqueCount = 1;
         for(int i=0; i<n; i++){
-            if(!ans.contains(nums[i])){
-                    ans.add(nums[i]);
-                }
+            if(nums[i] != nums[uniqueCount-1]){
+                nums[uniqueCount]  = nums[i];
+                uniqueCount++;
             }
-        for(int i=0 ; i<ans.size();i++){
-            nums[i]=ans.get(i);
         }
-        return ans.size();
+        return uniqueCount;
     }
 }

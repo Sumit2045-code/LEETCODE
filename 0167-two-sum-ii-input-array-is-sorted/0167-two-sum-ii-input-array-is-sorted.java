7d90e16bacb2db128b58ec2932f0697d3542f0f7
Sumit2045-code <sumitcodes2045@gmail.com>
2026-03-15 12:21:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -2,9 +2,7 @@ class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int left = 0;
         int right = numbers.length - 1;
-
         while(left < right){
-
             if(numbers[left] + numbers[right] < target){
                 left++;
             }
@@ -14,7 +12,6 @@ class Solution {
             else{
                 return new int[]{left + 1, right + 1};
             }
-
         }
         return new int[]{-1,-1};
     }

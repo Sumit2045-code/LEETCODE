39e79c6af51d3cc5d39c4b7d97077e59f6c5410d
Sumit2045-code <sumitcodes2045@gmail.com>
2026-03-15 12:03:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,12 +1,32 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int n = numbers.length;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(numbers[i]+numbers[j] == target)
-                return new int[]{i+1,j+1};
-            }
+        // int n = numbers.length;
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(numbers[i]+numbers[j] == target)
+        //         return new int[]{i+1,j+1};
+        //     }
+        // }
+        // return new int[]{-1,-1};
+
+        // two pointer approach for this code is the best solution and here it is 
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while(left < right){
+
+            int sum = numbers[left] + numbers[right];
+
+            if(sum == target)
+                return new int[]{left+1, right+1};
+
+            else if(sum < target)
+                left++;
+
+            else
+                right--;
         }
+
         return new int[]{-1,-1};
     }
 }

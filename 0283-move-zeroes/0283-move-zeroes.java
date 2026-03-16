96cdc9ed3a9df572959cc9168faf7a9e67690c1b
Sumit2045-code <sumitcodes2045@gmail.com>
2026-03-16 12:28:29 +0530

    Time: 2 ms (90.48%), Space: 47.7 MB (72.54%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n =nums.length;
+        int i=0;
+        for(int j=0;j<n;j++){
+           if(nums[j]!=0){
+            swap(nums,i,j);
+            i++;
+           }
+        }
+    }
+    public static void swap(int[] nums,int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}

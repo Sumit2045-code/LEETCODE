6e273e44bfc424a528dcdb20931e10b31b723730
Sumit2045-code <sumitcodes2045@gmail.com>
2026-04-24 04:12:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,23 +1,18 @@
 class Solution {
     public boolean isPalindrome(String s) {
         int i = 0, j = s.length() - 1;
-
         while (i < j) {
-
             // skip invalid characters
             while (i < j && !Character.isLetterOrDigit(s.charAt(i))) i++;
             while (i < j && !Character.isLetterOrDigit(s.charAt(j))) j--;
-
             // compare lowercase
             if (Character.toLowerCase(s.charAt(i)) != 
                 Character.toLowerCase(s.charAt(j))) {
                 return false;
             }
-
             i++;
             j--;
         }
-
         return true;
     }
 }

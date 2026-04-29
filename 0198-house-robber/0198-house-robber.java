1f7d7dd63637f4d18527ff311a328bcab468ceb6
Sumit2045-code <sumitcodes2045@gmail.com>
2026-04-29 18:01:41 +0530

    Time: 0 ms (100%), Space: 43 MB (19.11%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,18 @@
+class Solution {
+    static int[] dp;
+    public int rob(int[] nums) {
+        int n = nums.length;
+        dp = new int[n];// 0 to n-1
+        Arrays.fill(dp,-1);//mark 
+        return loot(0,nums);
+    }
+    private int loot(int i,int[] nums){
+        if(i>=nums.length) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int pick = nums[i]+loot(i+2,nums);
+        int skip = loot(i+1,nums);
+        int ans = Math.max(pick,skip);
+        dp[i] = ans;
+        return ans;
+    }
+}

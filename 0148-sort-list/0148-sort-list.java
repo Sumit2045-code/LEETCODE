d96e2952c89b59bd12c8618557653f4d60183991
Sumit2045-code <sumitcodes2045@gmail.com>
2026-07-26 10:02:25 +0530

    Time: 9 ms (98.21%), Space: 59.5 MB (37.58%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,37 @@
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null) return head; //BASE CASE
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode head2 = slow.next;
+        slow.next = null; // this is the line which breaks the linkedlist
+
+        ListNode head1 = sortList(head);
+        head2 = sortList(head2);
+        return merge(head1,head2);
+    }
+    public ListNode merge(ListNode head1, ListNode head2) {
+        ListNode i = head1;
+        ListNode j = head2;
+        ListNode dummy = new ListNode(-1);
+        ListNode k = dummy;
+        while(i!=null && j!=null){
+            if(i.val<=j.val){
+                k.next = i;
+                i = i.next;
+            }
+            else{
+                k.next = j;
+                j = j.next;
+            }
+            k = k.next;
+        }
+        if(i==null) k.next = j;
+        else k.next = i;
+        return dummy.next;
+    }
+}

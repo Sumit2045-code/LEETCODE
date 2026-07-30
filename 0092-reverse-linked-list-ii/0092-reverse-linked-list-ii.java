0fbfa1178977a85f6986f7428e46c7a43fef646e
Sumit2045-code <sumitcodes2045@gmail.com>
2026-07-30 10:24:06 +0530

    Time: 0 ms (100%), Space: 42.9 MB (67.54%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -14,7 +14,7 @@ class Solution {
         ListNode dummy = new ListNode(-1);
         ListNode temp = dummy;
         dummy.next = head;
-
+    //in dono for loops ne make sure kiya ki hmare 4 pointers bn jae
         for(int i=1;i<=left-1;i++){
             temp=temp.next;
         }
@@ -26,12 +26,16 @@ class Solution {
         }
         ListNode tail2 = temp;
         ListNode head3 = temp.next;
+
+    // yha pe linkedlist ko break krte hai hm 
         tail1.next = null;
         tail2.next = null;
-
+    // us part ko revrse kr diye 
         reverse(head2);
+    // ab firse jod diye finally
         tail1.next=tail2;
         head2.next=head3;
+    // return krdo dummy ka next 
         return dummy.next;
     }
 }

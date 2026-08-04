4e0291c827fdb3a3472ddbd456de1c5f503d75ed
Sumit2045-code <sumitcodes2045@gmail.com>
2026-08-04 11:26:22 +0530

    Time: 38 ms (41.63%), Space: 47.3 MB (23.71%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,12 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
+        int[] ans = new int[]{-1,-1};
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 if(nums[i]+nums[j] == target)
                 return new int[]{i,j};
             }
         }
-        return new int[]{-1,-1};
+        return ans;
     }
 }

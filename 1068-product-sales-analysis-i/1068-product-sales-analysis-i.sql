12564ce3487c957589ef8b1513061286b49b07da
Sumit2045-code <sumitcodes2045@gmail.com>
2026-08-16 01:37:31 +0530

    Time: 1568 ms (17.93%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,7 +1,5 @@
 # Write your MySQL query statement below
-SELECT product.product_name,
-       sales.year,sales.price
-FROM sales 
-INNER JOIN 
-product 
-ON sales.product_id=product.product_id
+SELECT p.product_name, s.year, s.price
+FROM Sales as s
+JOIN Product as p
+ON S.PRODUCT_ID=P.PRODUCT_ID

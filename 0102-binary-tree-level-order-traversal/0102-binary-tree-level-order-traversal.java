304a92f61cd7b632b8f84960402bbe86d395eea7
Sumit2045-code <sumitcodes2045@gmail.com>
2026-09-07 18:25:46 +0530

    Time: 16 ms (1.61%), Space: 46.9 MB (41.28%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,35 +1,24 @@
 class Solution {
-    class Pair {
-        TreeNode node;
-        int level;
-        Pair(TreeNode node, int level) {
-            this.node = node;
-            this.level = level;
-        }
-    }
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
         if (root == null) return ans;
-        Queue<Pair> q = new LinkedList<>();
-        int currLevel = 0;
-        q.add(new Pair(root, 0));
-        List<Integer> list = new ArrayList<>();
-        while (q.size() > 0) {
-            Pair front = q.remove();
-            if (front.level != currLevel) {
-                ans.add(list);
-                list = new ArrayList<>();
-                currLevel++;
-            }
-            list.add(front.node.val);
-            if (front.node.left != null) {
-                q.add(new Pair(front.node.left, front.level + 1));
-            }
-            if (front.node.right != null) {
-                q.add(new Pair(front.node.right, front.level + 1));
-            }
+        int level = 0;
+        while (true) {
+            List<Integer> curr = new ArrayList<>();
+            kthLevel(root, 0, level, curr);
+            if (curr.size() == 0) break;
+            ans.add(curr);
+            level++;
         }
-        ans.add(list);
         return ans;
+    }
+    private void kthLevel(TreeNode root, int level, int k, List<Integer> curr) {
+        if (root == null) return;
+        if (level == k) {
+            curr.add(root.val);
+            return;
+        }
+        kthLevel(root.left, level + 1, k, curr);
+        kthLevel(root.right, level + 1, k, curr);
     }
 }

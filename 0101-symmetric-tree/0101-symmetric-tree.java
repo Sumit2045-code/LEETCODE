df80957d53f6d634f21c56caf84b44640ce8b189
Sumit2045-code <sumitcodes2045@gmail.com>
2026-09-07 11:23:28 +0530

    Time: 0 ms (100%), Space: 43.5 MB (70.48%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -0,0 +1,22 @@
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null) return null;
+        TreeNode temp = root.left;
+        root.left=root.right;
+        root.right=temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+         if(p==null && q==null) return true;
+         if(p==null || q==null) return false;
+         if(p.val!=q.val) return false;
+         return isSameTree(p.left,q.left) && isSameTree(p.right,q.right) ; 
+    }
+    public boolean isSymmetric(TreeNode root) {
+        invertTree(root.left);
+        return isSameTree(root.left,root.right);
+    }
+
+}

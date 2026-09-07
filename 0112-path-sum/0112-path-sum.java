fe93bdf0b3baeba676000f45df7fa9f41ce2a7ca
Sumit2045-code <sumitcodes2045@gmail.com>
2026-09-07 15:08:26 +0530

    Time: 0 ms (100%), Space: 45.1 MB (31.12%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -3,6 +3,7 @@ class Solution {
         if(root==null) return false;
         if(root.left==null && root.right==null){
             if(targetSum-root.val==0) return true;
+            else return false;
         }
         return(hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val));
     }

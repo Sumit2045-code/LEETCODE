4bd5d82e4bd8700474de995ca3acd620810ba11b
Sumit2045-code <sumitcodes2045@gmail.com>
2026-09-11 00:17:12 +0530

    Time: 14 ms (98.39%), Space: 69.7 MB (79.24%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -1,16 +1,9 @@
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root==null) return null;
-        if(root.val==p.val || root.val==q.val) return root;
-        boolean pLiesInLeft = exists(root.left,p.val);
-        boolean qLiesInLeft = exists(root.left,q.val);
-        if(pLiesInLeft && qLiesInLeft) return lowestCommonAncestor(root.left,p,q);
-        else if(!pLiesInLeft && !qLiesInLeft) return lowestCommonAncestor(root.right,p,q);
-        else return root;
-    }
-    private boolean exists (TreeNode root, int value){
-        if(root==null) return false;
-        if(root.val==value) return true;
-        return exists(root.left,value) || exists(root.right,value);
+        if (root == null || root.val == p.val || root.val == q.val) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) return root;
+        return left != null ? left : right;
     }
 }
